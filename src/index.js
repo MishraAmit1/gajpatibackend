76477364aa0f7a300b77fed03eecb0f3d3fa91a7
MishraAmit1 <amsmishra1510@gmail.com>
2025-08-02 18:02:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "https://gajpatiadminfrontend.onrender.com",
+  "http://gajpati-backend.onrender.com",
   "http://localhost:5173",
   "http://localhost:5174",
 ];
