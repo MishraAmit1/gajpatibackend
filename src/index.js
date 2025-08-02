87e8e202d1c55e0156ae4d58d1f57d13b7a8327d
MishraAmit1 <amsmishra1510@gmail.com>
2025-08-02 19:06:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -56,6 +56,7 @@ app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
   "https://gajpatiadminfrontend.onrender.com",
   "https://gajpatifrontend.onrender.com/",
+  "https://gajpati-industries.netlify.app",
   "http://localhost:5173",
   "http://localhost:5174",
 ];
